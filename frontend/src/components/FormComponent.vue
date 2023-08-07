3cf85e0a2e898866c0381644c6aee1ca7b23db95
GaelleBriet <gaelle.briet@protonmail.com>
2023-08-07 12:57:33 +0200

    ✨ checkboxes

--- frontend/src/components/FormComponent.vue
+++ frontend/src/components/FormComponent.vue
@@ -2,6 +2,7 @@
 import TextInput from "../components/Input/TextInput.vue";
 import SelectInput from "../components/Input/SelectInput.vue";
 import { ref } from "vue";
+import CheckboxInput from "./Input/CheckboxInput.vue";
 
 const projectName = ref("");
 
@@ -19,37 +20,7 @@ const inputName = name => {
 
     <SelectInput project-type />
 
-    <div class="input-group">
-      <span class="main-label">Développement génériques</span>
-      <div class="checkbox-group">
-        <input id="homepage" type="checkbox" name="generic-templates[]" value="homepage" />
-        <label for="homepage"> Homepage </label>
-      </div>
-      <div class="checkbox-group">
-        <input id="contact" type="checkbox" name="generic-templates[]" value="contact" />
-        <label for="contact"> Contact </label>
-      </div>
-      <div class="checkbox-group">
-        <input id="blog" type="checkbox" name="generic-templates[]" value="blog" />
-        <label for="blog"> Blog </label>
-      </div>
-      <div class="checkbox-group">
-        <input id="boutique" type="checkbox" name="generic-templates[]" value="boutique" />
-        <label for="boutique"> Boutique </label>
-      </div>
-      <div class="checkbox-group">
-        <input id="editorial" type="checkbox" name="generic-templates[]" value="editorial" />
-        <label for="editorial"> Page éditorial </label>
-      </div>
-      <div class="checkbox-group">
-        <input id="events" type="checkbox" name="generic-templates[]" value="events" />
-        <label for="events"> Évènements </label>
-      </div>
-      <div class="checkbox-group">
-        <input id="jobs" type="checkbox" name="generic-templates[]" value="jobs" />
-        <label for="jobs"> Offres d'emplois </label>
-      </div>
-    </div>
+    <CheckboxInput />
 
     <div class="input-group">
       <span class="main-label">Développements spécifiques</span>
--- frontend/src/components/Input/CheckboxInput.vue
+++ frontend/src/components/Input/CheckboxInput.vue
@@ -1,7 +1,31 @@
-<script setup></script>
+<script setup>
+const checkboxes = [
+  { name: "Homepage", id: "homepage" },
+  { name: "Page de contact", id: "page-de-contact" },
+  { name: "Blog", id: "blog" },
+  { name: "Boutique", id: "boutique" },
+  { name: "Page éditorial", id: "editorial" },
+  { name: "Évènements", id: "evnements" },
+  { name: "Offres d'emplois", id: "module-meteo" }
+];
+</script>
 
 <template>
-  <h2>Composant : CheckboxInput.vue</h2>
+  <div class="input-group">
+    <span class="main-label">Développement génériques</span>
+    <template v-for="checkbox in checkboxes" :key="checkbox.id">
+      <div class="checkbox-group">
+        <input
+          :id="checkbox.id"
+          type="checkbox"
+          :name="checkbox.name"
+          :value="checkbox.name"
+          @change="handleCheckboxChange($event, checkbox.name)"
+        />
+        <label :for="checkbox.id"> {{ checkbox.name }} </label>
+      </div>
+    </template>
+  </div>
 </template>
 
 <style scoped></style>
