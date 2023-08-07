49941caecbb632e835fdab8308728ec761ee504d
Pablo <pablo93200@outlook.fr>
2023-08-07 14:10:54 +0200

    add ApiService and test in EstimateList

---
+++ frontend/src/services/ApiService.js
@@ -0,0 +1,54 @@
+const ApiService = {
+    async fetchAll(type) {
+        try {
+            const response = await fetch(`http://pabloprevot-server.eddi.cloud:8090/api/${type}`);
+            const data = await response.json();
+            return data;
+          } catch (error) {
+            throw new Error(`Failed to fetch resource of type : ${type}`);
+          }
+    },
+
+    async create(type, data) {
+        try {
+            const response = await fetch(`http://pabloprevot-server.eddi.cloud:8090/api/${type}`, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(data),
+            });
+            const responseData = await response.json();
+            return responseData;
+          } catch (error) {
+            throw new Error(`Failed to create resource of type : ${type}`);
+          }
+    },
+
+    async fetchOne(type, id) {
+        try {
+            const response = await fetch(`http://pabloprevot-server.eddi.cloud:8090/api/${type}/${id}`);
+            const data = await response.json();
+            return data;
+          } catch (error) {
+            throw new Error(`Failed to fetch resource of type : ${type} with id : ${id}`);
+          }
+    },
+
+    async delete(type, id) {
+        try {
+            const response = await fetch(`http://pabloprevot-server.eddi.cloud:8090/api/${type}/${id}`, {
+              method: 'DELETE',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            });
+            const responseData = await response.json();
+            return responseData;
+          } catch (error) {
+            throw new Error(`Failed to delete resource of type : ${type} with id : ${id}`);
+          }
+    },
+}
+
+export default ApiService;
