47c7c2afd9dcb44c368fec721857f18a223846ab
GaelleBriet <gaelle.briet@protonmail.com>
2023-08-07 13:40:20 +0200

    ✨ added specific templates component

--- frontend/src/components/Input/CustomTaskInput.vue
+++ frontend/src/components/Input/CustomTaskInput.vue
@@ -1,7 +1,109 @@
-<script setup></script>
+<script setup>
+// const addSpecificTemplateFields = () => {
+//   const specificTemplateList = document.getElementById("specificTemplateList");
+
+//   if (!specificTemplateList) {
+//     return;
+//   }
+//   const specificTemplateWrapper = document.createElement("div");
+//   specificTemplateWrapper.classList.add("specific-template");
+
+//   const specificTemplateWrapperRemoveBtn = document.createElement("button");
+//   specificTemplateWrapperRemoveBtn.className = "button button-icon";
+//   specificTemplateWrapperRemoveBtn.innerHTML = '<i class="fa fa-minus"></i>';
+
+//   specificTemplateWrapperRemoveBtn.addEventListener("click", () => {
+//     specificTemplateWrapper.remove();
+//   });
+
+//   // Specific Template Name
+//   const specificTemplateName = document.createElement("label");
+//   specificTemplateName.className = "specific-template-name";
+
+//   const specificTemplateInput = document.createElement("input");
+//   specificTemplateInput.type = "text";
+//   specificTemplateInput.name = "specific-template";
+
+//   const specificTemplateInputNamePrefix = document.createElement("span");
+//   specificTemplateInputNamePrefix.textContent = "Nom";
+//   specificTemplateInputNamePrefix.className = "specific-template-input-prefix";
+
+//   specificTemplateName.appendChild(specificTemplateInputNamePrefix);
+//   specificTemplateName.appendChild(specificTemplateInput);
+
+//   // Specific Template Time
+//   const specificTemplateTime = document.createElement("label");
+//   specificTemplateName.className = "specific-template-time";
+
+//   const specificTemplateTimeInput = document.createElement("input");
+//   specificTemplateTimeInput.type = "number";
+//   specificTemplateTimeInput.name = "specific-template-time";
+
+//   const specificTemplateInputUnit = document.createElement("span");
+//   specificTemplateInputUnit.textContent = "h";
+//   specificTemplateInputUnit.className = "specific-template-input-unit";
+
+//   specificTemplateTime.append(specificTemplateTimeInput);
+//   specificTemplateTime.append(specificTemplateInputUnit);
+
+//   // Add everything to the wrapper
+//   specificTemplateWrapper.appendChild(specificTemplateName);
+//   specificTemplateWrapper.appendChild(specificTemplateTime);
+
+//   specificTemplateWrapper.appendChild(specificTemplateWrapperRemoveBtn);
+
+//   specificTemplateList.appendChild(specificTemplateWrapper);
+// };
+
+import { ref } from "vue";
+
+const specificTemplates = ref([]);
+
+const addSpecificTemplate = () => {
+  specificTemplates.value.push({ name: "", time: "" });
+};
+
+const removeSpecificTemplate = index => {
+  specificTemplates.value.splice(index, 1);
+};
+</script>
 
 <template>
-  <h2>Composant : CustomTaskInput.vue</h2>
+  <div class="input-group">
+    <span class="main-label">Développements spécifiques</span>
+
+    <div class="specific-template-list">
+      <div v-for="(specificTemplate, index) in specificTemplates" :key="index" class="specific-template">
+        <label class="specific-template-name">
+          <span class="specific-template-input-prefix">Nom</span>
+          <input v-model="specificTemplate.name" type="text" />
+        </label>
+
+        <label class="specific-template-time">
+          <input v-model="specificTemplate.time" type="number" />
+          <span class="specific-template-input-unit">h</span>
+        </label>
+
+        <button class="button button-icon" @click="removeSpecificTemplate(index)">
+          <i class="fa fa-minus"></i>
+        </button>
+      </div>
+    </div>
+
+    <button class="button button-icon" type="button" @click="addSpecificTemplate">
+      <i class="fa fa-plus"></i>
+    </button>
+  </div>
 </template>
 
 <style scoped></style>
+
+<!-- <div class="input-group">
+  <span class="main-label">Développements spécifiques</span>
+
+  <div id="specificTemplateList" class="specific-template-list"></div>
+
+  <button id="addSpecificTemplate" class="button button-icon" type="button" @click="addSpecificTemplateFields">
+    <i class="fa fa-plus"></i>
+  </button>
+</div> -->
--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -1,11 +1,38 @@
 <script setup>
-import FormComponent from "../components/FormComponent.vue";
 import TitleComponent from "@/components/TitleComponent.vue";
+import TextInput from "../components/Input/TextInput.vue";
+import SelectInput from "../components/Input/SelectInput.vue";
+import { ref } from "vue";
+import CheckboxInput from "../components/Input/CheckboxInput.vue";
+import CustomTaskInput from "../components/Input/CustomTaskInput.vue";
+
+const projectName = ref("");
+
+const inputName = name => {
+  projectName.value = name.target.value;
+  console.log(projectName.value);
+};
+
+
 </script>
 
 <template>
   <TitleComponent title="Calculato'r" />
-  <FormComponent />
+  <form class="estimator-form" action="#">
+    <div class="errors">Le nom du projet est obligatoire.</div>
+
+    <TextInput @input="inputName" />
+
+    <SelectInput project-type />
+
+    <CheckboxInput />
+
+    <CustomTaskInput />
+
+    <SelectInput design-type />
+
+    <button type="submit" class="button">Obtenir l'estimation</button>
+  </form>
 </template>
 
 <style scoped></style>
